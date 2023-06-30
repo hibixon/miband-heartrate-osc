1fcf27ce5cc6199f81c3242717ad4bf4a76f0eba
Tayou <tayou@gmx.net>
2023-06-30 17:32:36 +0200

    remove number from project name (folder, namespace, etc.)

---
+++ MiBand-Heartrate/Extras/MessageWindow.cs
@@ -0,0 +1,25 @@
+using System.Windows;
+
+namespace MiBand_Heartrate.Extras
+{
+    public static class MessageWindow
+    {
+        public static string Title = "Heartrate Monitor";
+
+        public static MessageBoxResult Show(string message, MessageBoxButton buttons = MessageBoxButton.OK)
+        {
+            return MessageBox.Show(message, Title, buttons, MessageBoxImage.None);
+        }
+
+        public static MessageBoxResult ShowError(string message, MessageBoxButton buttons = MessageBoxButton.OK)
+        {
+            return MessageBox.Show(message, Title + " - Error", buttons, MessageBoxImage.Error);
+        }
+
+        public static MessageBoxResult ShowWarning(string message, MessageBoxButton buttons = MessageBoxButton.OK)
+        {
+            return MessageBox.Show(message, Title + " - Warning", buttons, MessageBoxImage.Warning);
+        }
+
+    }
+}
---
+++ MiBand-Heartrate/ViewModel.cs
@@ -0,0 +1,17 @@
+using System;
+using System.ComponentModel;
+
+namespace MiBand_Heartrate
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public ViewModel() { }
+
+        public void InvokePropertyChanged(string property)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(property));
+        }
+    }
+}
